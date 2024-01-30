31188ae0c8b8ee2250c38f0f2ebcc30d81ad95a4
sudhir gadpayle <sudhirgadpayle535@gmail.com>
2024-01-30 21:04:11 +0530

    testing diamention on vercel live

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,8 +27,8 @@ export default function Accordion(): JSX.Element {
   };
 
   return (
-    <main className=" w-full h-svh sm:h-screen relative before:content-[''] before:left-0 before:top-0 before:absolute before:w-full before:h-1/2 before:sm:h-[45%] before:bg-mobile before:sm:bg-desk before:-z-10 grid place-items-center">
-      <div className="w-[330px] min-h-52 sm:w-[500px] bg-white rounded-lg shadow-xl shadow-grayish-purple/20 p-5">
+    <main className=" w-full h-svh sm:h-screen relative before:content-[''] before:left-0 before:top-0 before:absolute before:w-full before:h-1/2 before:sm:h-[45%] before:bg-mobile before:sm:bg-desk before:-z-10 grid place-items-center transition-all ease-linear">
+      <div className="w-[330px] min-h-52 sm:w-[500px] bg-white rounded-lg shadow-xl shadow-grayish-purple/20 p-5 sm:p-6">
         {/* title */}
         <div className="flex justify-start items-center gap-x-3">
           <Image
@@ -40,15 +40,22 @@ export default function Accordion(): JSX.Element {
           <h3 className="text-dark-purple text-2xl font-bold">FAQs</h3>
         </div>
         {/* list accordiion container */}
-        <div className=" my-4 flex flex-col gap-3">
+        <div className=" my-4 flex flex-col gap-2">
           {accordionData.map(
             ({ title, desc }, key): JSX.Element => (
-              <div key={key}>
+              <div
+                key={key}
+                className={`border-b border-purple-200 py-2 last:border-none`}
+              >
                 <button
                   className=" w-full flex justify-between items-start sm:items-center sm:gap-x-2"
                   onClick={() => openDesc(key)}
                 >
-                  <h5 className="text-left text-sm sm:text-base font-semibold text-dark-purple hover:text-purple-800">
+                  <h5
+                    className={`text-left text-sm sm:text-base font-semibold text-dark-purple hover:text-purple-800 ${
+                      isOpen === key ? "text-purple-800" : "text-dark-purple"
+                    }`}
+                  >
                     {title}
                   </h5>
                   <Image
@@ -61,13 +68,13 @@ export default function Accordion(): JSX.Element {
                 <AnimatePresence>
                   {isOpen === key && (
                     <motion.div
-                      className="mt-2"
+                      className="mt-1"
                       variants={openTransition}
                       initial="initial"
                       animate="animate"
                       exit="exit"
                     >
-                      <p className="text-xs sm:text-sm text-grayish-purple text-justify pr-5">
+                      <p className="text-xs sm:text-sm  text-dark-purple text-justify pr-5">
                         {desc}
                       </p>
                     </motion.div>
@@ -78,18 +85,18 @@ export default function Accordion(): JSX.Element {
           )}
         </div>
       </div>
-      <p className="absolute bottom-0 text-xs p-4 text-zinc-800">
+      <p className="absolute bottom-0 text-xs p-5 text-zinc-800">
         Challange By{" "}
         <a
           href="https://www.frontendmentor.io/"
-          className="underline underline-offset-2 decoration-slate-400 font-semibold"
+          className="underline underline-offset-2 decoration-slate-400 font-semibold hover:text-purple-700"
         >
           Frontend Mentor
         </a>{" "}
         and coded by{" "}
         <a
           href="https://www.frontendmentor.io/profile/w3Scribe"
-          className="underline underline-offset-2 decoration-slate-400 font-semibold"
+          className="underline underline-offset-2 decoration-slate-400 font-semibold hover:text-purple-700"
         >
           Sudhir Gadpayle
         </a>
